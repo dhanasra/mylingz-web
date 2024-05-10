3842bb8faba9ee5464478b3a04cba9e82c673b09
dhanasra <dhana.spiderlingz@gmail.com>
2024-05-10 15:23:14 +0530

    update: biolink

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -24,21 +24,21 @@ export const themeColors = [
     "#27ae60", // Emerald
 ];
 
-export const lightenColor = (hex, percent) => {
-    console.log(hex);   
-    if(hex){
-        return '';
-    }
-    let num = parseInt(hex.slice(1), 16);
-    let amt = Math.round(2.55 * percent);
-    let R = ((num >> 16) + amt) & 0xFF; // Enclose the expression in parentheses
-    let G = ((num >> 8) & 0x00FF) + amt; // Enclose the expression in parentheses
-    let B = (num & 0x0000FF) + amt;
+// export const lightenColor = (hex, percent) => {
+//     console.log(hex);   
+//     if(hex){
+//         return '';
+//     }
+//     let num = parseInt(hex.slice(1), 16);
+//     let amt = Math.round(2.55 * percent);
+//     let R = ((num >> 16) + amt) & 0xFF; // Enclose the expression in parentheses
+//     let G = ((num >> 8) & 0x00FF) + amt; // Enclose the expression in parentheses
+//     let B = (num & 0x0000FF) + amt;
 
-    console.log(`#${(1 << 24 | R << 16 | G << 8 | B).toString(16).slice(1)}`)
+//     console.log(`#${(1 << 24 | R << 16 | G << 8 | B).toString(16).slice(1)}`)
 
-    return `#${(1 << 24 | R << 16 | G << 8 | B).toString(16).slice(1)}`;
-};
+//     return `#${(1 << 24 | R << 16 | G << 8 | B).toString(16).slice(1)}`;
+// };
 
 export const gradients = {
     "0": "linear-gradient(to bottom right, #bdc3c7, #2c3e50)",
